bc74be1a5e5b9d58be8057f49e44ddb166def778
lamanhvu <anv2003bmt@gmail.com>
2024-01-06 22:06:48 +0700

    finish bai2 thread

--- Learn_Thread/bai2/main.c
+++ Learn_Thread/bai2/main.c
@@ -9,7 +9,7 @@
 
 pthread_t thread_id1, thread_id2, thread_id3;
 int fd;
-char str[100],str2[100];
+char buffer[300];
 pthread_mutex_t lock1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t lock2 = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
@@ -21,17 +21,22 @@ typedef struct
     char hometown[30];
 }thr_human;
 thr_human human;
+int ready = 0;
+int finished = 0;
 
 static void *thr_handle1(void *args)
 {
+    thr_human *thr = (thr_human *)args;
     while (1)
     {
         sleep(1);
-        thr_human *thr = (thr_human *)args;
         pthread_mutex_lock(&lock1);
         //critical section 
         printf("Name:");
         fgets(human.name,sizeof(human.name),stdin);
+        if(strcmp(human.name,"exit\n")==0){         //go exit de thoat
+            exit(EXIT_SUCCESS);
+        }
         human.name[strcspn(human.name, "\n")] = '\0';
         printf("Birth:");
         fgets(human.birth,sizeof(human.birth),stdin);
@@ -40,6 +45,7 @@ static void *thr_handle1(void *args)
         fgets(human.hometown,sizeof(human.hometown),stdin);
         human.hometown[strcspn(human.hometown, "\n")] = '\0';
 
+        ready=1;
         pthread_cond_signal(&cond);
         pthread_mutex_unlock(&lock1);
     }  
@@ -48,26 +54,25 @@ static void *thr_handle1(void *args)
 
 static void *thr_handle2(void *args)
 {
+    thr_human *thr = (thr_human *)args;
     while (1)
     {
-        thr_human *thr = (thr_human *)args;
-        fd=open("thongtinsinhvien.txt",O_CREAT|O_RDWR,0666);
+        pthread_mutex_lock(&lock1);
+        // ready in advance when pthread_cond_signal() is called
+        pthread_cond_wait(&cond, &lock1);
+
+        fd=open("thongtinsinhvien.txt",O_CREAT|O_WRONLY|O_APPEND,0666);
         if(fd==-1){
             perror("open()");
             exit(EXIT_FAILURE);
         }
-        pthread_mutex_lock(&lock1);
 
-        // ready in advance when pthread_cond_signal() is called
-        pthread_cond_wait(&cond, &lock1);
-
-        char buffer[300];
         int length=snprintf(buffer,sizeof(buffer),"%s | %s | %s\n",human.name,human.birth,human.hometown);
         printf("%d\n",length);
 
         ssize_t bytesWritten = write(fd, buffer, length);
         if (bytesWritten == -1) {
-            perror("write");
+            perror("write()");
         }
 
         close(fd);
@@ -80,26 +85,43 @@ static void *thr_handle2(void *args)
 
 static void *thr_handle3(void *args)
 {
+    thr_human *thr = (thr_human *)args;
     while (1)
     {
-        thr_human *thr = (thr_human *)args;
         pthread_mutex_lock(&lock1);
         // ready in advance when pthread_cond_signal() is called
         pthread_cond_wait(&cond2, &lock1);
 
-        fd=open("thongtinsinhvien.txt",O_CREAT|O_RDWR,0666);
+        fd=open("thongtinsinhvien.txt",O_RDONLY);
         if(fd==-1){
             perror("open()");
             exit(EXIT_FAILURE);
         }
-        
-        char buffer[300];
+        // Set con tro toi vi tri truoc ki tu '\n' o dong cuoi
+        off_t currentPosition = lseek(fd, -2, SEEK_END);
 
         ssize_t bytesRead;
-        if ((bytesRead=read(fd,buffer,sizeof(buffer))>0)) {
-            printf("%s",buffer);
-        }
 
+        // Ghi tung ki tu cho den khi gap ki tu '\n' o dong ke cuoi
+        int i = 0;
+        char c;
+        while ((bytesRead = read(fd, &c, 1)) == 1 && c != '\n' && currentPosition >= 0) {
+            buffer[i++] = c;
+            currentPosition = lseek(fd, currentPosition - 1, SEEK_SET);
+        }
+        buffer[i] = '\n';
+
+        // Switch chuoi
+        int j = 0;
+        int k = i - 1;
+        while (j < k) {
+            char temp = buffer[j];
+            buffer[j] = buffer[k];
+            buffer[k] = temp;
+            j++;
+            k--;
+        }
+        printf("%s",buffer);
         close(fd);
         pthread_mutex_unlock(&lock1);
     }
@@ -108,22 +130,23 @@ static void *thr_handle3(void *args)
 
 int main(int argc, char *argv[]){
     int ret;
-
-    if(ret = pthread_create(&thread_id1,NULL,&thr_handle1,NULL)){
-        perror("pthread_create() 1 error");
+    
+    if(ret = pthread_create(&thread_id3,NULL,&thr_handle3,&human)){
+        perror("pthread_create() 3 error");
         return -1;
     }
     if(ret = pthread_create(&thread_id2,NULL,&thr_handle2,NULL)){
         perror("pthread_create() 2 error");
         return -1;
     }
-    if(ret = pthread_create(&thread_id3,NULL,&thr_handle3,&human)){
-        perror("pthread_create() 3 error");
+    if(ret = pthread_create(&thread_id1,NULL,&thr_handle1,NULL)){
+        perror("pthread_create() 1 error");
         return -1;
     }
+    
     // used to block for the end of a thread and release
     pthread_join(thread_id1,NULL);  
     pthread_join(thread_id2,NULL);
-    // pthread_join(thread_id3,NULL);
+    pthread_join(thread_id3,NULL);
     return 0;
 }
